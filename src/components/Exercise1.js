6b4b9f9024635edad9d66aeaf6379a6454f8ffc4
tawfiq <tawfiq_kh@yahoo.com>
2021-06-27 12:00:37 +0300

    finish

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -8,14 +8,30 @@ class Exercise1 extends Component {
             name: "",
             age: ""
         }
+        this.change=this.change.bind(this);
+        this.alertText=this.alertText.bind(this);
     }
 
+    change(e){
+        let inputVal = e.target.value;
+        let id = e.target.id;
+        if(id==="name-input"){
+            this.setState({name:inputVal})
+        }
+        else{
+            this.setState({age:inputVal})
+        }
+    }
+
+    alertText(){
+        alert("Come in "+ this.state.name+", you’re " + this.state.age +"- that’s good enough");
+    }
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" value={this.state.name} onChange={this.change}/>
+                <input id="age-input" value={this.state.age} onChange={this.change}/>
+                <button onClick={this.alertText}>Go to Bar</button>
             </div>
         );
     }
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -8,13 +8,29 @@ constructor() {
         name: "",
         fruit: ""
     }
+    this.changeVal=this.changeVal.bind(this);
 }
 
+async changeVal(e){
+    let inputVal= e.target.value;
+    let id = e.target.id;
+    if(id==="name-input"){
+        this.setState({name:inputVal});
+    }
+    else{
+       await this.setState({fruit:inputVal});
+       console.log(this.state.name + " selected "+this.state.fruit);
+    }
+}
 render() {
     return (
         <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
+            <input id="name-input" value={this.state.name} onChange={this.changeVal}/>
+            <select id="select-input" onChange={this.changeVal}>  
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option></select>
         </div>
     );
 }
